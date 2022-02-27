abd2d938c99900415bfb3d4ad130400e02add641
Keith Compere <keithcompere150@gmail.com>
2022-02-27 00:14:19 -0500

    added the getters for hero and started abstract class enemy

--- Hero.java
+++ Hero.java
@@ -1,3 +1,46 @@
 public abstract class Hero {
+    protected int level;
+    protected int Hpcap;
+    protected int Hp;
+    protected int Strength;
+    protected int Defence;
+    protected int Vitality;
+    protected int Magic;
+    protected int Speed;
+    protected static int Balance;
+    protected int Exp;
+    protected int Expcap;
+
+    public int getLevel(){
+        return(this.level);
+    }
+    public int getHpcap(){
+        return(this.Hpcap);
+    }
+    public int getHp(){
+        return(this.Hp);
+    }    
+    public int getStrength(){
+        return(this.Strength);
+    }    
+    public int getDefence(){
+        return(this.Defence);
+    }   
+     public int getVitality(){
+        return(this.Vitality);
+    }   
+     public int getMagic(){
+        return(this.Magic);
+    }    
+    public int getSpeed(){
+        return(this.Speed);
+    }
+    public int getBalance(){
+        return(this.Balance);
+    }
     
+    public int getExp(){
+        return(this.Exp);
+    }   
+
 }
