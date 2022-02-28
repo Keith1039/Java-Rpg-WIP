bbecb219cf41b6ac7ffaedf80503583290e4f85f
Keith Compere <keithcompere150@gmail.com>
2022-02-27 21:07:54 -0500

    Enemy abstract class mostly done

--- Enemy.java
+++ Enemy.java
@@ -1,3 +1,46 @@
 public abstract class Enemy {
-    
+    protected int level;
+    protected String name;
+    protected Status status;
+    protected int Hpcap;
+    protected int Hp;
+    protected int Strength;
+    protected int Defence;
+    protected int Vitality;
+    protected int Speed;
+    protected int Exp;
+    protected int moneyget;
+
+    public int getLevel(){
+        return(this.level);
+    }
+    public Status getStatus(){
+        return(this.status);
+    }
+    public int getHpcap(){
+        return(this.Hpcap);
+    }
+    public int getHp(){
+        return(this.Hp);
+    }    
+    public int getStrength(){
+        return(this.Strength);
+    }    
+    public int getDefence(){
+        return(this.Defence);
+    }   
+     public int getVitality(){
+        return(this.Vitality);
+    }   
+    public int getSpeed(){
+        return(this.Speed);
+    }
+    public int getExp(){
+        return(this.Exp);
+    }   
+    public void attack(Hero other){
+        
+    }
+    public abstract void action(Hero other);
+
 }
