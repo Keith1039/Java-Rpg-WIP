ef62b6bcb24d9c03cdb53dc76e11e29a8744399b
Keith Compere <keithcompere150@gmail.com>
2022-03-26 13:31:31 -0400

    Messing around with JFrame

--- game.java
+++ game.java
@@ -1,6 +1,9 @@
+import javax.swing.JFrame;
 public class game{
-
+    
     public static void main(String[] args){
+        JFrame frame = new JFrame("Rpg");
+        frame.setVisible(true);
         Assassin test_assassin=new Assassin();
         Goblin test_Goblin=new Goblin();
         test_assassin.attack(test_Goblin);
--- utils.java
+++ utils.java
@@ -1,4 +1,7 @@
 import java.util.Random;
+import java.awt.*; 
+import javax.swing.*;
+import javax.swing.JFrame;
 public class utils{
 
 }
