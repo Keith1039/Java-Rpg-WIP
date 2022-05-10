f970540cf264ce859e827f15c3b5a6c8166f6cda
Keith Compere <keithcompere150@gmail.com>
2022-05-10 19:56:26 -0400

    messing with save and load. Load is pain...

--- game.java
+++ game.java
@@ -1,7 +1,10 @@
 import java.util.Random;
+import java.util.Scanner;
 import java.io.*;
 public class game{
     private int gamestate=0;
+    private int money=0;
+    //private String[] items = null; probably make a list function for items
     public static void main(String[] args) throws IOException{
         game nice= new game();
         
@@ -21,12 +24,16 @@ public class game{
         TestDummy dummy = new TestDummy();
         Linkedlist turns=new Linkedlist();
 
+        /*
         for(int i=0; i<999;i++){
         mark.levelup();
         }
+        */
         
         //System.out.println(mark.printMovelist());
         nice.save(Des, helena, Ried, mark);
+
+        nice.load(new File("Save"));
         //mark.levelup();
         //nice.save(Des, helena, Ried, mark);
         /*
@@ -52,6 +59,7 @@ public class game{
 
     public void save(Assassin assassin,Whitemage whitemage,Blackmage blackmage, Warrior warrior) throws IOException{
         Hero[] hirro = new Hero[]{assassin,whitemage,blackmage,warrior};
+        
         try{
             File f= new File("Save");
             if(f.exists()==false){
@@ -60,6 +68,7 @@ public class game{
             FileWriter writer = new FileWriter(f);
             PrintWriter printWriter = new PrintWriter(writer);
             printWriter.println(Integer.toString(this.gamestate));
+            printWriter.println(Integer.toString(this.money));
             for(int i=0;i<hirro.length;i++){
                 printWriter.println(hirro[i].toString());
                 printWriter.flush();
@@ -71,12 +80,87 @@ public class game{
         }catch(IOException e){
             System.out.println("Error occured");
         }
+
+        
     }
 
-    public Object[] load(File saveFile){
-        Object[] objects =new Object[5];
+    public Object[] load(File saveFile) throws IOException{ // only shows up if save file exists
+        Assassin assassin=null;
+        Blackmage blackmage=null;
+        Whitemage whitemage=null;
+        Warrior warrior=null;
+        Hero[] hirros=new Hero[]{assassin,whitemage,blackmage,warrior};
+        Object[] objects =new Object[6]; //first 2 are ints, the others are gonna be the hero objects
+        Scanner scanner= new Scanner(saveFile);
+        
+        int tracker=0;
+        int tracker2=0;
+        objects[0]=Integer.parseInt(scanner.nextLine());
+        objects[1]=Integer.parseInt(scanner.nextLine());
+
+        int level;
+        String name;
+        Status status;
+        int Hpcap;
+        int Hp;
+        int Strength;
+        int Defence;
+        int Vitality;
+        int Magic;
+        int Speed;
+        int Exp;
+        int Expcap;
+
+        
+        while(scanner.hasNext()==true){ //every 13 it switches to a new character, lines 2,7 don't matter
+            tracker2++;
+            System.out.println(tracker2);
+            //System.out.println(scanner.nextLine());
+            if(tracker2==1){
+                name=scanner.nextLine();
+                String actual_name="";
+                char[] array=name.toCharArray();
+                boolean condition =false;
+
+                for(int i =0; i<array.length;i++){
+                    
+
+                if(condition == true){
+                    actual_name+=array[i];
+                }
+                if(array[i]==' '){
+                    condition=true;
+                }
+                }
+            }
+            if(tracker2==5){
+                String returnable = digitstring(scanner.nextLine());
+                level=Integer.parseInt(returnable);
+                //level = Integer.valueOf(returnable);
+                //System.out.println(level);
+
+            }
+        }
+        scanner.close();
+
 
         return(objects);
     }
+
+    public String digitstring(String string){
+        char[] returnables = string.toCharArray();
+        String digitstring ="";
+
+        for(int i=0; i<returnables.length;i++){
+            Character test = returnables[i];
+            if(test.isDigit(returnables[i])==true){
+                digitstring+=returnables[i];
+            }
+        }
+
+        return(digitstring);
+    }
+
 }
+
 
